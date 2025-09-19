ba0a866883cc36fd6961c0f3a456540a097eb004
ankdoss <cepl@CEPLs-MacBook-Pro.local>
2025-09-19 14:31:38 +0530

    added

--- dist/component/dto/create-component.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CreateComponentDto {
-    name: string;
-    code: string;
-    description?: string;
-}
--- dist/component/dto/update-component.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UpdateComponentDto {
-    name?: string;
-    code?: string;
-    description?: string;
-}
